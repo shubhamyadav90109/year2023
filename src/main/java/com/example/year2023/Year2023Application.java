1a773f2282f00a6f9760ad5e942828cf5298752f
shubhamyadav <shubhamyadav90109@gmail.com>
2023-05-11 14:27:20 +0530

    code-refactor

---
+++ src/main/java/com/example/year2023/Year2023Application.java
@@ -0,0 +1,13 @@
+package com.example.year2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Year2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Year2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/year2023/Year2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.year2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Year2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
